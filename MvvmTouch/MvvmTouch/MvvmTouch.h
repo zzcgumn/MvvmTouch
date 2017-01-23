af943c4738b94a62a5286454ab610ad9582c8922
Martin Nygren <m@rtin-nygren.se>
2017-01-23 19:47:15 +0000

    Defines a MvvmTouch library target.

---
+++ MvvmTouch/MvvmTouch/MvvmTouch.h
@@ -0,0 +1,19 @@
+//
+//  MvvmTouch.h
+//  MvvmTouch
+//
+//  Created by Martin Nygren on 23/01/2017.
+//  Copyright © 2017 Martin Nygren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MvvmTouch.
+FOUNDATION_EXPORT double MvvmTouchVersionNumber;
+
+//! Project version string for MvvmTouch.
+FOUNDATION_EXPORT const unsigned char MvvmTouchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MvvmTouch/PublicHeader.h>
+
+
