960940e9c0fa5c53b5274b2426151ff0e59a0a8b
Martin Nygren <m@rtin-nygren.se>
2017-01-28 18:20:32 +0000

    Changes the initial view controller to be a navigation controller.

--- SimplePushPresentExample/SimplePushPresentExample/AppDelegate.swift
+++ SimplePushPresentExample/SimplePushPresentExample/AppDelegate.swift
@@ -15,6 +15,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        return true
+
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            if let viewController = storyboard.instantiateInitialViewController() {
+                let navController = UINavigationController(rootViewController: viewController)
+                window = UIWindow(frame: UIScreen.main.bounds)
+                window?.rootViewController = navController
+                window?.makeKeyAndVisible()
+                return true
+            }
+
+        return false
     }
 }
