9fae145e75ca36677aa2229bc5ec6bec1a03b5d7
Martin Nygren <m@rtin-nygren.se>
2017-07-05 23:18:33 +0100

    Defines an example project for showing how flows work.

--- MvvmTouch/FlowExample/FlowExample/ColoredTableViewController.swift
+++ MvvmTouch/FlowExample/FlowExample/ColoredTableViewController.swift
@@ -1,4 +1,12 @@
 //
+//  ColoredTableViewController.swift
+//  FlowExample
+//
+//  Created by Martin Nygren on 05/07/2017.
+//  Copyright © 2017 Martin Nygren. All rights reserved.
+//
+
+//
 //  ViewController.swift
 //  TableViewExample
 //
