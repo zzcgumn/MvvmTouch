506e7158247a666ed4534444f7f7be6cc944673e
Martin Nygren <m@rtin-nygren.se>
2017-07-15 13:55:54 +0100

    Makes it easier to reuse example view controllers.

--- Examples/MvvmExampleUI/ColoredTableViewController.swift
+++ Examples/MvvmExampleUI/ColoredTableViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ColoredTableViewController.swift
 //  TableViewExample
 //
 //  Created by Martin Nygren on 05/02/2017.
@@ -35,7 +35,7 @@ class ColoredCellSection: MvvmTableViewSection<ColoredCellModel, ColoredCellType
     }
 }
 
-class ViewController: MvvmCellModelTableViewController {
+class ColoredTableViewController: MvvmCellModelTableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
